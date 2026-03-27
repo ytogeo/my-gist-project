0dfa26fb78b6e81fe7d2f907d197a0bd796db6d6
Yutong Yang <125036680+ytogeo@users.noreply.github.com>
2026-03-27 21:22:58 +0800

    Obscure API key in gaode-amap.js
    Obscured the API key in gaode-amap.js for security.

--- public/js/gaode-amap.js
+++ public/js/gaode-amap.js
@@ -18,7 +18,7 @@ var autoOptionsOfDestination = {
 };
 
 AMapLoader.load({
-    key: "dc9bc33aee58a34ea63ec1e2991aefc1", // 申请好的Web端开发者Key，首次调用 load 时必填
+    key: "********************", // 申请好的Web端开发者Key，首次调用 load 时必填
     "plugins": ['AMap.AutoComplete'],
     version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
 }).then((AMap) => {
